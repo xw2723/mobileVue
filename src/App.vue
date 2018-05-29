037b955bb2ac62805be6e440c8bfed7c7a703adb
xiaowei <xiaowei@gridsum.com>
2018-05-29 21:56:31 +0800

    1111

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <dome1/>
+  </div>
+</template>
+
+<script>
+import dome1 from './components/dome1'
+
+export default {
+  name: 'App',
+  components: {
+    dome1
+  }
+}
+</script>
+
+<style>
+html {
+  font-size: 16px;
+}
+body {
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#app * {
+  /* font-size: 1rem; */
+}
+</style>
---
+++ src/components/dome1.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <mt-header title="销售KPI">
+      <mt-button icon="close" slot="left"></mt-button>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+
+    <div>
+      <mt-navbar v-model="view.timeType">
+        <mt-tab-item v-for="(item, index) in metadata.timeTypeList" :key="index" :id="item.key">{{item.name}}</mt-tab-item>
+      </mt-navbar>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      view: {
+        timeType: 5
+      },
+      metadata: {
+        timeTypeList: [
+          {
+            key: 1,
+            name: '昨日'
+          },
+          {
+            key: 2,
+            name: '上周'
+          },
+          {
+            key: 3,
+            name: '本周'
+          },
+          {
+            key: 4,
+            name: '上月'
+          },
+          {
+            key: 5,
+            name: '本月'
+          },
+          {
+            key: 6,
+            name: '年至今'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+</style>
